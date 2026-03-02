e14c8677d4fe6beb88b657292273ed37a6153b3d
Camila Brito <camilam.brito@outlook.com.br>
2026-03-01 22:19:45 -0300

    Ajustando a interface para realizar a edição de pensamentos.

--- js/api.js
+++ js/api.js
@@ -8,7 +8,6 @@ const api = {
             throw error;
         }
     },
-
     async salvarPensamento(pensamento){
         try{
             const response = await fetch('http://localhost:3000/pensamentos',{
@@ -20,10 +19,34 @@ const api = {
             });
             return await response.json();
         } catch {
-            alert('Erro ao buscar pensamentos');
+            alert('Erro ao salvar pensamento');
             throw error;
         }
-    }
+    },
+    async buscarPensamentoPorId(id){
+        try{
+            const response = await fetch(  `http://localhost:3000/pensamentos/${id}`);
+            return await response.json();
+        }catch {
+            alert('Erro ao buscar pensamento');
+            throw error;
+        }
+    },
+    async editarPensamento(pensamento){
+        try{
+            const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`,{
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(pensamento)
+            });
+            return await response.json();
+        } catch {
+            alert('Erro ao editar pensamento');
+            throw error;
+        }
+    },
 
 }
 
