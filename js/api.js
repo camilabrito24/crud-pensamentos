82631be4a4986e58c26fe63055472d6a200126c9
Camila Brito <camilam.brito@outlook.com.br>
2026-03-08 23:56:25 -0300

    Ajustando a interface para realizar a exclusão de pensamentos.

--- js/api.js
+++ js/api.js
@@ -47,7 +47,16 @@ const api = {
             throw error;
         }
     },
-
+    async excluirPensamento(id){
+        try{
+            const response = await fetch(`http://localhost:3000/pensamentos/${id}`,{
+                method: "DELETE",
+            });
+        } catch {
+            alert('Erro ao excluir pensamento');
+            throw error;
+        }
+    }
 }
 
 export default api;
